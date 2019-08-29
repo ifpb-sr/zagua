3a124d7a8fe05d7f6a2e48af7439a3276cb2c47e
allanabl <lanaterui@gmail.com>
2019-08-29 10:19:55 -0300

    criar banco de dados COMERCIANTE #7

--- zagua/models.py
+++ zagua/models.py
@@ -7,7 +7,16 @@ db = SQLAlchemy()
 class Usuario(db.Model):
     __tablename__ = 'usuarios'
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), primary_key=True)
+    username = db.Column(db.String(50), primary_key=True, index=True)
     senha = db.Column(db.String(64), unique = True)
+
     def __repr__(self):
-        return '<id: %r, username: %r, senha: %r>' % self.id, self.username, self.senha
+        return '<Usuario %r>' self.username
+
+class Loja(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(60), unique=True, index=True)
+
+    def __repr__(self):
+        return 'Loja %r' self.name
+
