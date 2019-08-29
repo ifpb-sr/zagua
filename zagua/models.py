d2948cf6aa467c0705a1a9e44e57c13e3b30a16a
allanabl <lanaterui@gmail.com>
2019-08-29 10:31:31 -0300

    criar banco de dados COMERCIANTE #7

--- zagua/models.py
+++ zagua/models.py
@@ -9,14 +9,15 @@ class Usuario(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), primary_key=True, index=True)
     senha = db.Column(db.String(64), unique = True)
-
+    loja_id = db.Column(db.Integer, db.ForeignKey('Loja.id'))
     def __repr__(self):
         return '<Usuario %r>' self.username
 
 class Loja(db.Model):
+    __tablename__ = 'lojas'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(60), unique=True, index=True)
-
+    usuarios = db.relationship('Usuario', backref='loja')
     def __repr__(self):
         return 'Loja %r' self.name
 
