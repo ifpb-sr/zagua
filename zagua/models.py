e1caec804834bd2d17a69a7c72c03a7159ec8a48
allanabl <lanaterui@gmail.com>
2019-08-29 09:10:54 -0300

    criar banco de dados COMERCIANTE #7

--- zagua/models.py
+++ zagua/models.py
@@ -4,13 +4,9 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
-    __tablename__ = 'users'
+class Usuario(db.Model):
+    __tablename__ = 'usuarios'
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(128), unique=True, index=True)
     senha = db.Column(db.String(64))
-    telefone = db.Column(db.String(32))
-    nome = db.Column(db.String(128))
-
     def __repr__(self):
         return '<User %r>' % self.email
