0a7a9d60a9be7c09f76bea731ce54ab997874031
allanabl <lanaterui@gmail.com>
2019-09-05 09:15:10 -0300

    #7

--- zagua/models.py
+++ zagua/models.py
@@ -9,7 +9,6 @@ class Usuario(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), primary_key=True, index=True)
     senha = db.Column(db.String(64), unique = True)
-    loja_id = db.Column(db.Integer, db.ForeignKey('Loja.id'))
 
     def __repr__(self):
         return '<Usuario %r>' self.username
@@ -17,8 +16,8 @@ class Usuario(db.Model):
 class Loja(db.Model):
     __tablename__ = 'lojas'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(60), unique=True, index=True)
-    usuarios = db.relationship('Usuario', backref='loja')
+    nome = db.Column(db.String(60), unique=True, index=True)
+    dono_id = db.Column(db.Integer, db.ForeignKey('Usuario.id'))
 
     def __repr__(self):
         return 'Loja %r' self.name
---
+++ zagua/novo.py
@@ -0,0 +1,15 @@
+from flask import Flask, Blueprint, render_template
+from zagua import db
+from . import bp
+
+@bp.route('/user')
+def usuario():
+    return "<h1>aqui vai ta o codigo html</h1>"
+
+@bp.route('/loja')
+def loja():
+    return "<h1>aqui vai ta o codigo html</h1>"
+
+@bp.route('/novo')
+def novo_usuario():
+    return render_template('users/novo.html')
