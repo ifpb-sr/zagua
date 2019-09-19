509ca8ad4562dd574aa0711ed26074479525dbb0
Alefe de Lima Moreira <delimaalefe@gmail.com>
2019-09-19 20:01:10 -0300

    feat(module/view): views(routes) inside `views` directory
    put all of views in /views directory
    
    Solves issue #45

--- zagua/__init__.py
+++ zagua/__init__.py
@@ -19,7 +19,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] =\
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-#app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 
 bootstrap = Bootstrap(app)
 db = SQLAlchemy(app)
