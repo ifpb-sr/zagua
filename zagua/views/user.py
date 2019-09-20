ad51410d4dbb503bedbdbf6c5e2bff2e884b990f
Alefe de Lima Moreira <delimaalefe@gmail.com>
2019-09-19 21:42:59 -0300

    feat(CRUD operations+imports zagua): modification in name of functions of routes
    Solves issue #45

--- zagua/views/user.py
+++ zagua/views/user.py
@@ -1,17 +1,17 @@
 from zagua import app
 
 @app.route('/users/novo')
-def usuario_novo():
+def novo():
     return render_template('users/novo.html')
 
 @app.route('/users/listar')
-def loja():
+def listar():
     return "<h1>aqui vai ta o codigo para listar</h1>"
 
 @app.route('/users/atualizar')
-def novo_usuario():
+def atualizar():
     return "<h1>aqui vai ta o codigo para atualizar</h1>"
 
 @app.route('/users/apagar')
-def novo_usuario():
+def apagar():
     return "<h1>aqui vai ta o codigo para apagar</h1>"
