6b796cc84f1d5b64a5f0bf1263b2ea919b1517dd
Alefe de Lima Moreira <delimaalefe@gmail.com>
2019-09-19 21:28:53 -0300

    feat(routes/CRUD operations): modification and creation of routes of CRUD operations
    routes of CRUD operations in users
    
    Solves issue #45

--- zagua/views/user.py
+++ zagua/views/user.py
@@ -1,13 +1,17 @@
 from zagua import app
 
-@app.route('/user')
-def usuario():
-    return "<h1>aqui vai ta o codigo html</h1>"
-
-@app.route('/loja')
-def loja():
-    return "<h1>aqui vai ta o codigo html</h1>"
-
-@app.route('/novo')
-def novo_usuario():
+@app.route('/user/novo')
+def usuario_novo():
     return render_template('users/novo.html')
+
+@app.route('/user/listar')
+def loja():
+    return "<h1>aqui vai ta o codigo para listar</h1>"
+
+@app.route('/user/atualizar')
+def novo_usuario():
+    return "<h1>aqui vai ta o codigo para atualizar</h1>"
+
+@app.route('/user/apagar')
+def novo_usuario():
+    return "<h1>aqui vai ta o codigo para apagar</h1>"
