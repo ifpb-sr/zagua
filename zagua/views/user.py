ada870e1f8df15eb55fb3d62a091fe39aaf446fa
Alefe de Lima Moreira <delimaalefe@gmail.com>
2019-09-19 21:31:10 -0300

    feat(routes): modification all user -> users
    Solves issue #45

--- zagua/views/user.py
+++ zagua/views/user.py
@@ -1,17 +1,17 @@
 from zagua import app
 
-@app.route('/user/novo')
+@app.route('/users/novo')
 def usuario_novo():
     return render_template('users/novo.html')
 
-@app.route('/user/listar')
+@app.route('/users/listar')
 def loja():
     return "<h1>aqui vai ta o codigo para listar</h1>"
 
-@app.route('/user/atualizar')
+@app.route('/users/atualizar')
 def novo_usuario():
     return "<h1>aqui vai ta o codigo para atualizar</h1>"
 
-@app.route('/user/apagar')
+@app.route('/users/apagar')
 def novo_usuario():
     return "<h1>aqui vai ta o codigo para apagar</h1>"
