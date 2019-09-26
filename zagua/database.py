a0fa0c145ec249e96de537a1558f7ec018da26a9
Alefe de Lima Moreira <delimaalefe@gmail.com>
2019-09-26 13:44:16 +0000

    #49

--- zagua/database.py
+++ zagua/database.py
@@ -11,8 +11,5 @@ Base = declarative_base()
 Base.query = db_session.query_property()
 
 def init_db():
-    # import all modules here that might define models so that
-    # they will be registered properly on the metadata.  Otherwise
-    # you will have to import them first before calling init_db()
     import zagua.models
     Base.metadata.create_all(bind=engine)
