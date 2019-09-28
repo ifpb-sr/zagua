6318cc53a169642c92f5aabe796471fb40d0c528
Alefe de Lima Moreira <delimaalefe@gmail.com>
2019-09-28 14:50:54 -0300

    fix(base/templates): conserta os templates: base.html e o login.html Conserta a base.html em com o bloco head_files e o login tirando tags desnecessárias Solves no issue

--- zagua/views.py
+++ zagua/views.py
@@ -1,18 +1,22 @@
 from zagua import app
 from flask import Flask, render_template
 
-@app.route('/users/novo')
+@app.route('/users/create')
 def novo():
     return render_template('usuarios/novo.html')
 
-@app.route('/users/listar')
+@app.route('/users/read')
 def listar():
     return "<h1>aqui vai ta o codigo para listar</h1>"
 
-@app.route('/users/atualizar')
+@app.route('/users/update')
 def atualizar():
     return "<h1>aqui vai ta o codigo para atualizar</h1>"
 
-@app.route('/users/apagar')
+@app.route('/users/delete')
 def apagar():
     return "<h1>aqui vai ta o codigo para apagar</h1>"
+
+@app.route('/users/login')
+def login():
+    return render_template("/login/login.html")
