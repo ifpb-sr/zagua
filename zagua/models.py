1b13f931e7720fd8ceb36c2259d6f37cd7548d49
Alefe de Lima Moreira <delimaalefe@gmail.com>
2019-09-29 14:13:12 -0300

    feat(banco de dados): create model of Comerciante Model of Comerciante is create and with him, the model of telefones Solves issue #49

--- zagua/models.py
+++ zagua/models.py
@@ -5,45 +5,34 @@ from zagua.database import Base
 
 db = SQLAlchemy()
 
-class Usuario(Base):
-    __tablename__ = 'usuarios'
-    id = db.Column(db.Integer, primary_key=True)
-    #username = db.Column(db.String(50), primary_key=True, index=True)
-    username = db.Column(db.String(50), unique=True)
-    email = db.Column (db.String(100), unique=True)
+class Comerciante(Base):
+    __tablename__ = 'comerciantes'
+    email = db.Column(db.String(100), unique=True, primary_key=True)
     senha = db.Column(db.String(16))
-    cidade = db.Column(db.String(100))
-    estado = db.Column(db.String(100))
-    endereço = db.Column(db.String(150))
-    pontoReferencia = db.Column(db.String(100))
-    
-    telefones = db.relationship('telefones', backref='usuarios')
+    nome = db.Column(db.String(64))
 
-    def __init__ (self, username, senha, telefone, email, cidade="", estado="", endereco="", pontoReferencia=""):
-        self.username = username
-        self.senha = senha
-        self.telefone = telefone
+    telefones = db.relationship('Telefone', backref='comerciantes')
+
+    def __init__ (self, email, senha, nome):
         self.email = email
-        self.cidade = cidade
-        self.estado = estado
-        self.endereco = endereco
-        self.pontoReferencia = pontoReferencia
+        self.senha = senha
+        self.nome = nome
+        #return f"novo comerciante: {self.nome}"
 
     def __repr__(self):
-        return '<Usuario %r>' %self.username
-        
+        return '<Comerciante: %r>' %self.nome
+
 class Telefone(Base):
     __tablename__ = 'telefones'
     telefone = db.Column(db.String(11), unique=True, primary_key=True)
 
-    usuario = db.Column(db.Integer, db.ForeignKey('usuarios.id'))
+    comerciante = db.Column(db.String(100), db.ForeignKey('comerciantes.email'))
 
 class Loja(db.Model):
     __tablename__ = 'lojas'
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(60), unique=True, index=True)
     dono_id = db.Column(db.Integer, db.ForeignKey('Usuario.id'))
-
 
     def __repr__(self):
         return 'Loja %r' %self.name
@@ -53,7 +42,9 @@ from zagua.database import init_db
 init_db()
 from zagua.database import db_session
 from zagua.models import Usuario
+from zagua.models import Comerciante
 u = Usuario(username='alefemoreira', senha='123')
+c = Comerciante(email="alefe@123.com", senha="Senha@123", nome="Álefe")
 db_session.add(u)
 db_session.commit()
 '''
